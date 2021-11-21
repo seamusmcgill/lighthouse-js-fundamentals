0ac12f3e226c0bb9722e2d281777406e49afb5fe
Seamus McGill <seamusmcgill@gmail.com>
2021-11-20 19:30:02 -0500

    Experimented wit creating an array of ice cream flavours

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+// Add root beer to flavours and print all
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// Print first flavour
+console.log(iceCreamFlavours[0]);
+// Print last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+// Print number of flavours
+console.log(iceCreamFlavours.length);
